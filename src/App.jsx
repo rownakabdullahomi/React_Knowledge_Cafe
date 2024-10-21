fd34e47435548ed01cdb8e08898c08d23ec6035f
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-10-21 23:44:03 +0600

    blog component done layout fixed proptype error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css'
 import Blogs from './Components/Blogs/Blogs'
+import Bookmarks from './Components/Bookmarks/Bookmarks'
 import Header from './Components/Header/Header'
 
 function App() {
 
   return (
-    <>
+    <div className='w-11/12 mx-auto'>
       <Header></Header>
-      <Blogs></Blogs>
-    </>
+      <div className='md:flex'>
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
+
+    </div>
   )
 }
 
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,10 @@
+import profile from "../../assets/images/profile.png"
+
 const Header = () => {
     return (
-        <div>
+        <div className="py-4 border-b-2 flex justify-between items-center ">
             <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
+            <img src={profile} alt="" />
         </div>
     );
 };
