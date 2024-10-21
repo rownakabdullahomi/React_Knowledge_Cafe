b559addfde74248868c5f349718851244f19bb36
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-10-22 00:03:35 +0600

    blog data displayed in ui by fetching

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,10 +1,28 @@
 import PropTypes from 'prop-types';
 
 const Blog = ({blog}) => {
-    console.log(blog);
+    const {title, cover, author, author_img, posted_date, reading_time, hashtags} = blog;
     return (
         <div>
-            
+            <img src={cover} alt={`Cover of ${title}`} />
+            <div className='flex justify-between'>
+                <div className='flex gap-6'>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a>#{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
